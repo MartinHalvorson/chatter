070c18f55586d08d1313e43318ccc443b873a872
MTHal <14mthalvorson@gmail.com>
2016-10-19 02:00:16 -0500

    initialize_basics.py should now autofill basics.txt with most common words from Wikipedia

--- initialize_basics.py
+++ initialize_basics.py
@@ -1,38 +1,49 @@
 import wikipedia
 import json
 import re
-import requests
 
-
+# Selects the number of most common words to list
 top_n_basic_words = 200
 
+# Loads dictionary of most common words from basics_counts.txt
 with open("basics_counts.txt", "r") as fileName:
-    counts = json.load(fileName)
-    
-tally = 1
+    count_of_words = json.load(fileName)
 
-while True:
+for i in range(5000):
     try:
+        # Uses Wikipedia API to get random article (object)
         title = wikipedia.random(1)
-        print(str(tally) + ". " + title)
-        for word in re.findall("[0-9A-z']+", wikipedia.page(title).content.lower()):     
-            counts[word] = counts.get(word, 0) + 1
+
+        # Prints article number and title
+        print(str(i + 1) + ". " + title)
+
+        # Creates string of lowercase text from Wikipedia article
+        article_text = wikipedia.page(title).content.lower()
+
+        # Iterates through words in article text
+        for word in re.findall("[0-9A-z']+", article_text):
+            count_of_words[word] = count_of_words.get(word, 0) + 1
+
+    # Occasional errors retrieving file
     except:
-        print("Error, skipping")
-    tally += 1
-    if tally % 100 == 0:
-        basics = [(v, k) for v, k in counts.items()]
-        basics = sorted(basics, key = lambda x:x[1], reverse = True)[:top_n_basic_words]
+        print("Error, skipping " + title)
+
+    # Updates basics.txt file with most recent list every nth times (n = 5 below)
+    if i % 10 == 0:
+
+        # Creates list of words (v) and their count (k)
+        basics = [(v, k) for v, k in count_of_words.items()]
+        
+        # Sorts list in descending order, truncates based on number of words wanted
+        basics = sorted(basics, key=lambda x:x[1], reverse=True)[:top_n_basic_words]
+        
+        # Creates list of just words (removing count)
         basics = [x[0] for x in basics]
 
-        # Update basics
-        open('basics.txt', 'w').close() # Clears file
+        # Dumps list in json format into basics.txt file
+        with open('basics.txt', 'w') as file_name:
+            json.dump(basics, file_name)
 
-        with open('basics.txt', 'w') as file: # Saves dictionary with 100 most common words to basics.txt as json list
-            json.dump(basics, file)
-
-        # Updates counts
-        open("basics_counts.txt", "w").close()
-
-        with open("basics_counts.txt", "w") as fileName:
-            json.dump(counts, fileName)
+        # Updates basics_counts.txt to reflect updated dictionary (building over time instead of erasing after run)
+        with open("basics_counts.txt", "w") as file_name:
+            json.dump(count_of_words, file_name)
