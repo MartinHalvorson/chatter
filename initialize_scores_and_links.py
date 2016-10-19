75b107712c45628a9598b2bec4ff42bb98c176ad
MTHal <14mthalvorson@gmail.com>
2016-10-19 18:54:04 -0500

    initialize_scores_and_links.py can now initialize the scores dictionary from Wikipedia articles.

--- initialize_scores_and_links.py
+++ initialize_scores_and_links.py
@@ -3,7 +3,7 @@ import json
 import re
 
 # Selects number of Wikipedia articles to add
-number_of_articles = 11
+number_of_articles = 2001
 
 # Loads dictionary of most common words from basics_counts.txt
 with open("scores.txt", "r") as file_name:
@@ -17,6 +17,7 @@ with open("basics.txt", "r") as file_name:
 
 for i in range(number_of_articles):
     try:
+
         # Uses Wikipedia API to get random article (object)
         title = wikipedia.random(1)
 
@@ -25,18 +26,17 @@ for i in range(number_of_articles):
 
         # Creates string of lowercase text from Wikipedia article
         article_text = wikipedia.page(title).content.lower()
-        article_text = "this is a test sentence for analysis is a test"
 
         # Creates list of words in article
-        words = re.findall("[0-9A-z']+", article_text)
+        list_of_words = re.findall("[0-9A-z']+", article_text)
 
         # UPDATING LINKS DICTIONARY
         # Iterating through each word and its following word
-        for j in range(len(words) - 1):
+        for j in range(len(list_of_words) - 1):
 
             # Renaming for code clarity
-            first_word = words[j]
-            second_word = words[j + 1]
+            first_word = list_of_words[j]
+            second_word = list_of_words[j + 1]
 
             # Extracts first word's dictionary from links dictionary
             connected_words = links.get(first_word, {})
@@ -48,13 +48,35 @@ for i in range(number_of_articles):
             links[first_word] = connected_words
 
         # UPDATING SCORES DICTIONARY
-        for word in basics
+        # Removes basic words from list_of_words, renames it as list_of_keywords
+        list_of_keywords = [word for word in list_of_words if word not in basics]
 
+        # Iterating through each word in keywords (avoiding some at both ends for convenience)
+        for k in range(2, len(list_of_keywords) - 2):
 
+            # Renaming for code clarity
+            base_word = list_of_keywords[k]
+            prior_one = list_of_keywords[k - 1]
+            prior_two = list_of_keywords[k - 2]
+            later_one = list_of_keywords[k + 1]
+            later_two = list_of_keywords[k + 2]
+
+            # Extracts base_words connections from scores dictionary
+            connection = scores.get(base_word, {})
+
+            # Updates surrounding word's scores in base word's dictionary
+            connection[prior_two] = connection.get(prior_two, 0) + 1
+            connection[prior_one] = connection.get(prior_one, 0) + 3
+            connection[later_one] = connection.get(later_one, 0) + 3
+            connection[later_two] = connection.get(later_two, 0) + 1
+
+            # Puts base word's dictionary back into scores dictionary
+            scores[base_word] = connection
 
     # Occasional errors retrieving file
     except:
         print("Error, skipping " + title)
+
 
     # Updates basics.txt file with most recent list every nth times (n = 5 below)
     if i % 10 == 0:
